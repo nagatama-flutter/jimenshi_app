1dd3189824faef3250ed3cc14d6f26423dd1221b
iwakaze81 <iwakaze81@gmail.com>
2024-11-02 17:27:35 +0900

    Tweak result page

--- app/lib/feature/contract/presentation/contract_result_success_page.dart
+++ app/lib/feature/contract/presentation/contract_result_success_page.dart
@@ -1,4 +1,5 @@
 import 'package:app/router/app_router.dart';
+import 'package:app/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
@@ -10,22 +11,28 @@ class ContractResultSuccessPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Text(
-            "成功",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
+          Text(
+            "見破り成功！！！",
+            style: Theme.of(context).textTheme.text32Semibold,
             textAlign: TextAlign.center,
           ),
-          FilledButton(
-            child: const Text("Next"),
-            onPressed: () {
-              context.router.replaceAll([const OnboardingRoute()]);
-            },
+          const SizedBox(height: 24),
+          const SizedBox(
+            height: 200,
+            child: Placeholder(),
+          ),
+          const SizedBox(height: 48),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: FilledButton(
+              child: const Text("勝利を噛み締めて次のゲームをプレイ"),
+              onPressed: () {
+                context.router.replaceAll([const OnboardingRoute()]);
+              },
+            ),
           ),
         ],
       ),
