b97ade1f2424e6cd3730395295f22ed3e6a19e69
koda <koda.masaru3@gmail.com>
2024-11-02 17:36:09 +0900

    Add text-to-speech processing to the server.

---
+++ app_server/relaunch.sh
@@ -0,0 +1,12 @@
+# 停止
+docker compose down
+# イメージをビルド
+docker compose build
+# 依存するDBやキャッシュの起動
+docker compose up -d postgres redis
+# イメージをビルドすると、前のビルドイメージのゴミが残るので消す
+docker system prune -f
+# マイグレーション実行（必要に応じて）
+docker compose up migrations
+# バックエンドサービスの起動
+docker compose up -d app https-portal
