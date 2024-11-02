fd2f8777ea55e5c62a6439a8a85137fd5148ca6a
Yoshimura Takumi <77924543+iwakaze81@users.noreply.github.com>
2024-11-03 07:48:15 +0900

    Tweak UI (#23)
    * Tweak ui
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Add video controller

--- app/lib/feature/contract/controller/audio_play_controller.dart
+++ app/lib/feature/contract/controller/audio_play_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:video_player/video_player.dart';
 
@@ -5,11 +7,25 @@ final audioPlayControllerProvider =
     Provider.autoDispose<AudioPlayController>((_) => AudioPlayController());
 
 class AudioPlayController {
-  Future<void> play(String audioFileUrl) async {
+  Future<void> play(
+    String audioFileUrl, {
+    VoidCallback? onPlay,
+    VoidCallback? onPlayEnd,
+  }) async {
     final videoController = VideoPlayerController.networkUrl(
       Uri.parse(audioFileUrl),
       videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
     );
+
+    videoController.addListener(() {
+      if (!videoController.value.isInitialized) {
+        return;
+      }
+      if (videoController.value.duration == videoController.value.position) {
+        onPlayEnd?.call();
+        return;
+      }
+    });
 
     await videoController.initialize();
 
@@ -17,5 +33,7 @@ class AudioPlayController {
     await videoController.setVolume(1.0);
 
     await videoController.play();
+
+    onPlay?.call();
   }
 }
